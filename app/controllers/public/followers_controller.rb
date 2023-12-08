48e91098515fa0a35063b2833c58ca7decf0ebce
ayame <144100281+Ayame918@users.noreply.github.com>
2023-12-08 08:29:04 +0000

    controller作成

---
+++ app/controllers/public/followers_controller.rb
@@ -0,0 +1,9 @@
+class Public::FollowersController < ApplicationController
+  def follow
+  end
+
+  
+  def followers
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,39 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :public do
+    get 'sessions/new'
+    get 'sessions/create'
+    get 'sessions/destroy'
+  end
+  namespace :public do
+    get 'registrations/new'
+    get 'registrations/create'
+  end
+  
+  namespace :admin do
+    get 'sessions/new'
+    get 'sessions/create'
+    get 'sessions/destroy'
+  end
+  
+    namespace :admin do
+    root to: 'homes#top'
+     get "admin/search" => "searches#search"
+    resources :users, only: [:mypage, :show, :edit, :update, :withdraw]
+    resources :posts, only: [:index, :show, :destroy]
+    resources :favorites, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
+    resources :bookmarks, only: [:index, :create, :destroy]
+    resources :tags, only: [:index, :create]
+    resources :followings, only: [:create, :destroy]
+    resources :followers, only: [:follow, :followers]
+  end
+  
+  scope module: :public do
+    root to: 'homes#top'
+    resources :posts, only: [:index, :show]
+    resources :tags, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :edit, :update]
+    resources :users, only: [:index, :show,:edit, :update]
+  end
 end
