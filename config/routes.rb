70c017d0c38614ee177fef08898c2b778aa15727
ayame <144100281+Ayame918@users.noreply.github.com>
2023-12-08 12:55:44 +0000

    マグレーションファイルとデバイスでの管理者とユーザー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   namespace :public do
     get 'sessions/new'
     get 'sessions/create'
---
+++ db/migrate/20231208105504_create_post_bookmarks.rb
@@ -0,0 +1,10 @@
+class CreatePostBookmarks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :post_bookmarks do |t|
+      t.integer :users_id
+      t.integer :post_id
+
+      t.timestamps
+    end
+  end
+end
